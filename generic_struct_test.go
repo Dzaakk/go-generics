ff95fd52b0c66514a28f366fab2de371c81da764
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-05-01 22:52:27 +0700

    generic method

--- generic_struct_test.go
+++ generic_struct_test.go
@@ -3,6 +3,8 @@ package gogenerics
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type Data[T any] struct {
@@ -10,10 +12,26 @@ type Data[T any] struct {
 	Second T
 }
 
+func (d *Data[_]) SayHello(name string) string {
+	return "Hello " + name
+}
+
+func (d *Data[T]) ChangeFirst(first T) T {
+	d.First = first
+	return d.First
+}
 func TestData(t *testing.T) {
 	data := Data[string]{
 		First:  "First Name",
 		Second: "Last Name",
 	}
 	fmt.Println(data)
+}
+func TestGenericMethod(t *testing.T) {
+	data := Data[string]{
+		First:  "First Name",
+		Second: "Last Name",
+	}
+	assert.Equal(t, "Name", data.ChangeFirst("Name"))
+	assert.Equal(t, "Hello User1", data.SayHello("User1"))
 }
